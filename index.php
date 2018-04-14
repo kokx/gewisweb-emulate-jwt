5dd472af3e5c8526bc5b02a640e0715810696c9b
Pieter Kokx <pieter@kokx.nl>
2018-04-14 15:08:43 +0200

    Finished JWT emulation

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+use Firebase\JWT\JWT;
+
+require 'vendor/autoload.php';
+
+$secret = 'TestJWTGEWIS';
+$lidnr = 1234;
+$url = 'http://localhost:3000/callback';
+
+$token = JWT::encode([
+    'iss' => 'Test',
+    'iat' => (new \DateTime())->getTimestamp(),
+    'exp' => (new \DateTime('+5 min'))->getTimestamp(),
+    'lidnr' => $lidnr
+], $key, 'HS256');
+
+header('Location: ' . $url . '?token=' . $token);
